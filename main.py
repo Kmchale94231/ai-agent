c84e96828154b6ed79e812bd9c1f70969770a744
Kai McHale <kaimchale@icloud.com>
2026-01-28 00:09:08 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+import os
+import argparse
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+
+load_dotenv()
+
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+
+def main():
+    print("Hello from ai-agent!")
+
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    messages = [
+        types.Content(
+            role="user",
+            parts=[types.Part(text=args.user_prompt)]
+        )
+    ]
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=messages,
+    )
+
+    print(response.text)
+
+    if args.verbose:
+        if response.usage_metadata is None:
+            raise RuntimeError("No usage metadata found in the response.")
+
+        print("\n--- Metadata ---")
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
+if __name__ == "__main__":
+    main()
